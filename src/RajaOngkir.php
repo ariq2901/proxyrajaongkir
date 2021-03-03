4dbaeed9d4a2800f9808250ca85af483769a606a
ariq2901 <48340625+ariq2901@users.noreply.github.com>
2021-03-03 14:59:17 +0700

    Update RajaOngkir.php

--- src/RajaOngkir.php
+++ src/RajaOngkir.php
@@ -7,15 +7,15 @@ use ariqjusuf\rajaongkirlaravel\app\Kota;
 use ariqjusuf\rajaongkirlaravel\app\Provinsi;
 
 class RajaOngkir {
-	public function Provinsi(){
+	public static function Provinsi(){
 		return new Provinsi;
 	}
 
-	public function Kota(){
+	public static function Kota(){
 		return new Kota;
 	}
 
-	public function Cost($attributes){
+	public static function Cost($attributes){
 		return new Cost($attributes);
 	}
 }
